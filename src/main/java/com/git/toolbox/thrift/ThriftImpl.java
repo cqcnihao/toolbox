59b736030e593483f61bb9db9a5fc4d70ca15f0f
poan <panbenxing@feellike21.com>
2017-08-14 19:30:20 +0800

    thrift test

---
+++ src/main/java/com/git/toolbox/thrift/ThriftImpl.java
@@ -0,0 +1,23 @@
+package com.git.toolbox.thrift;
+
+/**
+ * Created by poan on 2017/08/14.
+ */
+public class ThriftImpl {
+
+
+    public static void main(String[] args) {
+//        // init a thrift client
+//        ThriftClient thriftClient = new ThriftClientImpl(() -> Arrays.asList(//
+//                ThriftServerInfo.of("192.168.30.249", 9090), //
+//                ThriftServerInfo.of("192.168.30.249", 9091) //
+//                // or you can return a dynamic result.
+//        ));
+//        // get iface and call
+//        System.out.println(thriftClient.iface(CalculatorService.Client.class));
+    }
+
+
+
+
+}
---
+++ src/main/java/com/git/toolbox/thrift/client/exception/NoBackendException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.git.toolbox.thrift.client.exception;
+
+/**
+ * <p>
+ * NoBackendException class.
+ * </p>
+ */
+public class NoBackendException extends RuntimeException {
+
+    private static final long serialVersionUID = 1123124123141323L;
+
+}
