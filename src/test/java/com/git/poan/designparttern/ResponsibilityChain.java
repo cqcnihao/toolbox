6f2aa0c17f230854513761c8537a18f949f0dd2e
poan <panbenxing@feellike21.com>
2018-04-02 13:59:02 +0800

    使用枚举实现责任链模式

---
+++ src/test/java/com/git/poan/designparttern/ResponsibilityChain.java
@@ -0,0 +1,39 @@
+package com.git.poan.designparttern;
+
+/**
+ * Created by poan on 2018/04/02.
+ */
+public enum ResponsibilityChain {
+
+
+    FIRST {
+        @Override
+        public void dose() {
+            System.out.println("我要说，123456789； 然后接下来有请下一位");
+        }
+    },
+    SECOND {
+        @Override
+        public void dose() {
+            System.out.println("我要说 abcdefgh； 然后接下来有请下一位");
+        }
+    },
+    THIRD {
+        @Override
+        public void dose() {
+            System.out.println("我要说 @！#￥%……&*； 然后接下来有请下一位");
+        }
+    },
+
+    ;
+
+    public abstract void dose() ;
+
+
+    public static void main(String[] args) {
+        ResponsibilityChain[] chains = ResponsibilityChain.values();
+        for (ResponsibilityChain chain : chains) {
+            chain.dose();
+        }
+    }
+}
