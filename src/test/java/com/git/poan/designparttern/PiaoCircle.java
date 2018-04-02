f743aa1df8d59a0eb8f1f224ff3598bead8a0f87
poan <panbenxing@feellike21.com>
2018-04-02 19:00:17 +0800

    观察者模式——文案fix

--- src/test/java/com/git/poan/designparttern/PiaoCircle.java
+++ src/test/java/com/git/poan/designparttern/PiaoCircle.java
@@ -4,6 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// 1. 将女神的状态发布抽象为接口
+interface GoddessFollower {
+    void post(PostType type);
+}
+
+// 2. 将女神的状态类型使用枚举列举处理
+enum PostType {
+    Image(1, "自拍"),
+    Voice(2, "语音"),
+    Video(3, "视频"),;
+
+    private final int value;
+    public final String content;
+
+    PostType(int value, String content) {
+        this.value = value;
+        this.content = content;
+    }
+}
+
+// 3. 女神的跟随者实现状态发布接口，并对`2`中的各种状态做对应处理
+class Diaosi implements GoddessFollower {
+
+    @Override
+    public void post(PostType type) {
+        switch (type) {
+            case Image:
+                System.out.println("Diaosi评价：好看！");
+                break;
+            case Video:
+                System.out.println("Diaosi评价：好美！赞一个");
+                break;
+            case Voice:
+                System.out.println("Diaosi评价：好听！赞一个");
+        }
+    }
+}
+
+
+// 3. 女神的跟随者实现状态发布接口，并对`2`中的各种状态做对应处理
+class FuShuai implements GoddessFollower {
+
+    @Override
+    public void post(PostType type) {
+        switch (type) {
+            case Image:
+                System.out.println("FuShuai评价：小姐姐有空么？");
+                break;
+            case Video:
+                System.out.println("FuShuai评价：小姐姐有空么？");
+                break;
+            case Voice:
+                System.out.println("FuShuai评价：小姐姐有空么？");
+        }
+    }
+}
+
 /**
  * Created by poan on 2018/04/02.
  */
@@ -50,56 +107,4 @@ public class PiaoCircle {
 
     }
 
-}
-
-enum PostType {
-    Image(1, "自拍"),
-    Voice(2, "语音"),
-    Video(3, "视频"),;
-
-    private final int value;
-    public final String content;
-
-    PostType(int value, String content) {
-        this.value = value;
-        this.content = content;
-    }
-}
-
-interface GoddessFollower {
-    void post(PostType type);
-}
-
-class Diaosi implements GoddessFollower {
-
-    @Override
-    public void post(PostType type) {
-        switch (type) {
-            case Image:
-                System.out.println("Diaosi评价：好看！");
-                break;
-            case Video:
-                System.out.println("Diaosi评价：好美！赞一个");
-                break;
-            case Voice:
-                System.out.println("Diaosi评价：好听！赞一个");
-        }
-    }
-}
-
-class FuShuai implements GoddessFollower {
-
-    @Override
-    public void post(PostType type) {
-        switch (type) {
-            case Image:
-                System.out.println("FuShuai评价：小姐姐有空么？");
-                break;
-            case Video:
-                System.out.println("FuShuai评价：小姐姐有空么？");
-                break;
-            case Voice:
-                System.out.println("FuShuai评价：小姐姐有空么？");
-        }
-    }
 }
